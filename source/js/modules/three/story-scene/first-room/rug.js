313fbb6117007bd271b3e7de0c672d05dc4605cc
Val Altynova <val.altynova@gmail.com>
2021-10-21 09:14:05 +0300

    Использование кастомного материала ShaderMaterial для процедурных текстур «Коврика» и «Дороги»

--- source/js/modules/three/story-scene/first-room/rug.js
+++ source/js/modules/three/story-scene/first-room/rug.js
@@ -1,21 +1,13 @@
 import * as THREE from 'three';
 
 import {getLathePointsForCircle, getLatheDegrees} from '../../helpers.js';
+import colors from '../../../helpers/colors.js';
 
 class Rug extends THREE.Group {
-  constructor() {
+  constructor({dark} = {}) {
     super();
 
-    this.getMaterial = (options = {}) => {
-      const {color, ...rest} = options;
-
-      return new THREE.MeshStandardMaterial({
-        color: new THREE.Color(color),
-        side: THREE.DoubleSide,
-        flatShading: true,
-        ...rest,
-      });
-    };
+    this.dark = dark;
 
     this.rug = {
       width: 180,
@@ -23,8 +15,10 @@ class Rug extends THREE.Group {
       radius: 763,
       degStart: 16,
       degEnd: 74,
-      color: `#A481D1`,
-      segments: 40,
+      mainColor: this.dark ? colors.ShadowedLightPurple : colors.LightPurple,
+      stripeColor: this.dark ? colors.ShadowedAdditionalPurple : colors.AdditionalPurple,
+      segments: 200,
+      stripes: 7,
     };
 
     this.addRug = this.addRug.bind(this);
@@ -32,12 +26,54 @@ class Rug extends THREE.Group {
     this.addRug();
   }
 
-  addRug() {
+  getMaterial() {
+    return new THREE.MeshStandardMaterial({
+      side: THREE.DoubleSide,
+      flatShading: true,
+      vertexColors: true,
+      color: new THREE.Color(this.rug.mainColor),
+    });
+  }
+
+  getGeometry() {
     const points = getLathePointsForCircle(this.rug.width, this.rug.depth, this.rug.radius);
     const {start, length} = getLatheDegrees(this.rug.degStart, this.rug.degEnd);
 
-    const rug = new THREE.LatheBufferGeometry(points, this.rug.segments, start, length);
-    const mesh = new THREE.Mesh(rug, this.getMaterial({color: this.rug.color}));
+    const rug = new THREE.LatheBufferGeometry(points, this.rug.segments, start, length).toNonIndexed();
+
+    const position = rug.attributes.position;
+    const positionArray = position.array;
+    const positionCount = position.count;
+
+    const stripeDegree = (this.rug.degEnd - this.rug.degStart) / this.rug.stripes;
+
+    const colorsFloor = [];
+    const color = new THREE.Color();
+
+    const beginning = new THREE.Vector3(positionArray[0], positionArray[1], positionArray[2]);
+
+    for (let i = 0; i < positionCount; i++) {
+      color.setStyle(this.rug.mainColor);
+
+      const vector = new THREE.Vector3(positionArray[i * 3], positionArray[i * 3 + 1], positionArray[i * 3 + 2]);
+      const angle = vector.angleTo(beginning) * THREE.Math.RAD2DEG;
+
+      const isStripe = Math.floor(angle / stripeDegree) % 2 === 1;
+
+      if (isStripe) {
+        color.setStyle(this.rug.stripeColor);
+      }
+
+      colorsFloor.push(color.r, color.g, color.b);
+    }
+
+    rug.setAttribute(`color`, new THREE.Float32BufferAttribute(colorsFloor, 3));
+
+    return rug;
+  }
+
+  addRug() {
+    const mesh = new THREE.Mesh(this.getGeometry(), this.getMaterial());
 
     this.add(mesh);
   }
